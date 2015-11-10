c959ccb0bccadaebcd71486c42b05567843661d7
nikhilc@bsf.io <nikhilc@bsf.io>
2015-11-10 12:34:17 +0530

    errors

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -112,7 +112,7 @@ install_db() {
 	fi
 
 	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	/usr/local/mysql/bin/mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
 install_wp
