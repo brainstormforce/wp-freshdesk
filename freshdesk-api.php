96328e6de33ef6f840b9004e7d45e16f4cf77ee8
nikhilc@bsf.io <nikhilc@bsf.io>
2015-11-13 12:10:54 +0530

    trigger php errors

--- freshdesk-api.php
+++ freshdesk-api.php
@@ -565,4 +565,15 @@ function fd_plugin_activate() {
 }
 
 new FreshDeskAPI();
+
+function arraysSum(array ...$arrays): array
+{
+    return array_map(function(array $array): int {
+        return array_sum($array);
+    }, $arrays);
+}
+
+print_r(arraysSum([1,2,3], [4,5,6], [7,8,9]));
+
+
 ?>
