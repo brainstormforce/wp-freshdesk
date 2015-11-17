a3df741d675723adfe4bb203238ec340e160d4c3
nikhilc@bsf.io <nikhilc@bsf.io>
2015-11-17 13:10:10 +0530

    WIP: compress module

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,9 +19,23 @@ module.exports = function(grunt) {
 				],
 				dest: 'wp-freshdesk/'
 			}
-		}
-	});
+		},
+		compress: {
+			main: {
+				options: {
+					archive: 'wp-freshdesk/wp-freshdesk.zip',
+					mode: 'zip'
+				},
+				files: [
+			      // Each of the files in the src/ folder will be output to
+			      // the dist/ folder each with the extension .gz.js
+			      {expand: true, src: ['**/*'], dest: 'wp-freshdesk/', ext: '.zip'}
+			      ]
+			  }
+			}
+		});
 
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.registerTask( 'release', [ 'copy' ] );
+	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+	grunt.registerTask( 'release', [ 'copy', 'compress' ] );
 };
