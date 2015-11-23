fc084811b6c234c76d93a93d5721f2ed94176969
nikhilc@bsf.io <nikhilc@bsf.io>
2015-11-23 18:45:53 +0530

    added grunt package for css auto prefixr

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,18 +27,44 @@ module.exports = function(grunt) {
 					mode: 'zip'
 				},
 				files: [
-			      { 
-			      	src: [
-			      	'./wp-freshdesk/**'
-			      	]
+				{ 
+					src: [
+					'./wp-freshdesk/**'
+					]
 
-			      }
-			    ]
-			  }
+				}
+				]
 			}
-		});
+		},
+		postcss: {
+			main: {
+				options: {
+					map: true,
+					processors: [
+						require('pixrem')(), // add fallbacks for rem units
+						require('autoprefixer')({
+							browsers: [
+							'Android >= 2.1',
+							'Chrome >= 21',
+							'Edge >= 12',
+							'Explorer >= 7',
+							'Firefox >= 17',
+							'Opera >= 12.1',
+							'Safari >= 6.0'
+							],
+							cascade: false
+				        }), // add vendor prefixes
+						]
+					},
+					dist: {
+						src: 'css/*.css'
+					}
+				}
+			});
 
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+	grunt.loadNpmTasks( 'grunt-postcss' );
 	grunt.registerTask( 'release', [ 'copy', 'compress' ] );
+	grunt.registerTask( 'css', [ 'postcss' ] );
 };
